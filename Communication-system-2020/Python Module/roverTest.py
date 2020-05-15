255b01d6fba42552036af6456efe79aaf1703e85
Pegasus2K <62468421+Pegasus2K@users.noreply.github.com>
2020-05-15 20:03:06 +0200

    Update roverTest.py

--- Communication-system-2020/Python Module/roverTest.py
+++ Communication-system-2020/Python Module/roverTest.py
@@ -1,12 +1,15 @@
 from connectionToGs import socket           # importa il modulo
+import time
+groundStation= socket()                     # si connette automaticamente alla groundstation
+while True:
+    print('collegato: '+str(groundStation.isConnected))
 
-groundStation= socket()                     # si connette alla groundstation con l'ip che ha trovato all'avvio e la porta standard
+    obj={"campo1":"xyz",                            # oggetto a caso
+        "campo2":2,
+        "sensore1":2.032,
+        "sensore2":20147
+        }
 
-obj={"campo1":0,                            # oggetto a caso
-    "campo2":2,
-    "sensore1":2.032,
-    "sensore2":20147
-    }
-
-groundStation.sendData(obj)                 # esempio di sendData
+    groundStation.sendData(obj)                 # esempio di sendData
+    time.sleep(2)
 groundStation.disconnect()                  # esempio di disconnect
